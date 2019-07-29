9cc76906c726d735be25ef7e780a5943a2666d8a
Bryan Hogan <bryanjhogan@outlook.com>
2019-07-29 14:44:07 -0400

    Beginning code

--- WeatherService/Controllers/WeatherController.cs
+++ WeatherService/Controllers/WeatherController.cs
@@ -7,26 +7,6 @@ namespace WeatherService.Controllers
     [Route("[controller]")]
     public class WeatherController : ControllerBase
     {
-        private readonly IHttpClientFactory _httpClientFactory;
 
-        public WeatherController(IHttpClientFactory httpClientFactory)
-        {
-            _httpClientFactory = httpClientFactory;
-        }
-
-        [HttpGet("{locationId}")]
-        public async Task<ActionResult> Get(int locationId)
-        {
-            var httpClient = _httpClientFactory.CreateClient("TemperatureService");
-            HttpResponseMessage httpResponseMessage = await httpClient.GetAsync($"temperature/{locationId}");
-
-            if (httpResponseMessage.IsSuccessStatusCode)
-            {
-                int temperature = await httpResponseMessage.Content.ReadAsAsync<int>();
-                return Ok(temperature);
-            }
-
-            return StatusCode((int)httpResponseMessage.StatusCode, await httpResponseMessage.Content.ReadAsStringAsync());
-        }
     }
 }
