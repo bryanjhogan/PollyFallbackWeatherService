b2f255271e030623f08700a8204eebb618b19da9
Bryan Hogan <bryanjhogan@outlook.com>
2019-07-29 14:40:38 -0400

    Finished code

---
+++ WeatherService/Startup.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Polly;
+
+namespace WeatherService
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            IAsyncPolicy<HttpResponseMessage> httpWaitAndRetryPolicy =
+                Policy.HandleResult<HttpResponseMessage>(r => !r.IsSuccessStatusCode)
+                    .WaitAndRetryAsync(3, retryAttempt => TimeSpan.FromSeconds(retryAttempt),
+                        ((result, span, retryCount, ctx) => Console.WriteLine($"Retrying({retryCount})...") )
+                    );
+
+            var fallbackPolicy =
+                Policy.HandleResult<HttpResponseMessage>(r => !r.IsSuccessStatusCode)
+                    .FallbackAsync(FallbackAction, OnFallbackAsync);
+
+            var wrapOfRetryAndFallback = Policy.WrapAsync(fallbackPolicy, httpWaitAndRetryPolicy);
+
+            services.AddHttpClient("TemperatureService", client =>
+            {
+                client.BaseAddress = new Uri("http://localhost:6001/");
+                client.DefaultRequestHeaders.Add("Accept", "application/json");
+            }).AddPolicyHandler(wrapOfRetryAndFallback);
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        private Task OnFallbackAsync(DelegateResult<HttpResponseMessage> response, Context context)
+        {
+            Console.WriteLine("About to call the fallback action. This is a good place to do some logging");
+            return Task.CompletedTask;
+        }
+
+        private Task<HttpResponseMessage> FallbackAction(DelegateResult<HttpResponseMessage> responseToFailedRequest, Context context, CancellationToken cancellationToken)
+        {
+            Console.WriteLine("Fallback action is executing");
+
+            HttpResponseMessage httpResponseMessage = new HttpResponseMessage(responseToFailedRequest.Result.StatusCode)
+            {
+                Content = new StringContent($"The fallback executed, the original error was {responseToFailedRequest.Result.ReasonPhrase}")
+            };
+            return Task.FromResult(httpResponseMessage);
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
